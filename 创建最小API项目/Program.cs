21ed992df11c1b078272bd41e4b8f3e2b92c1284
PolarisLBL <1512739280@qq.com>
2025-09-14 17:39:12 +0800

    24-自定义数据种子逻辑

--- 创建最小API项目/Program.cs
+++ 创建最小API项目/Program.cs
@@ -42,6 +42,37 @@ namespace MyBoards
                 dbContext.Database.Migrate(); //执行所有待执行的迁移
             }
 
+            //除了数据模型方法和手动调整迁移外，还可以直接引用MyBoard的数据库上下文类，按自定义逻辑输入一些初始数据
+            var users = dbContext.Users.ToList();
+            //如果Users表中没有任何数据，就添加2条初始数据
+            if (!users.Any())
+            {
+                var user1 = new User()
+                {
+                    Email = "user1@test.com",
+                    FullName = "User One",
+                    Address = new Address()
+                    {
+                        City = "Warszwa",
+                        Street = "Szeroka",
+                    }
+                };
+
+                var user2 = new User()
+                {
+                    Email = "user2@test.com",
+                    FullName = "User Two",
+                    Address = new Address()
+                    {
+                        City = "Krakow",
+                        Street = "Dluga",
+                    }
+                };
+
+                dbContext.Users.AddRange(user1,user2);
+                dbContext.SaveChanges();
+            }
+
             app.Run();
         }
     }
